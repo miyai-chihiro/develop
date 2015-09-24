9612a44a1e681f7322b917c1791c22b79a444e14
宮井千尋 <chihiro.urabe@gmail.com>
2015-09-24 17:05:38 +0900

    開発用のsrc (/ejs , /scss) の作成

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,30 @@ gulp.task("server", function() {
 
 //sass
 gulp.task('style', function() {
-    return sass('scss/',{
+    return sass('src/scss/',{
+      style: 'expanded',
+      compass: true,
+      sourcemap: true
+    })
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/css'))
+    .pipe(browser.reload({stream:true}));
+});
+
+//ejs
+gulp.task('ejs',function(){
+  gulp.src(
+    ['src/ejs/**/*.ejs','!' + 'src/ejs/**/_*.ejs']
+  )
+  .pipe(ejs())
+  .pipe(gulp.dest('src/public'))
+});
+
+//styledocco
+gulp.task('styledocco',function(){
+  gulp.src('styleguide/',function(){
+    return sass('src/scss/',{
       style: 'expanded',
       compass: true,
       sourcemap: true
@@ -27,32 +50,8 @@ gulp.task('style', function() {
     .pipe(autoprefixer())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css/'))
-    .pipe(browser.reload({stream:true}));
-});
-
-//ejs
-gulp.task('ejs',function(){
-  gulp.src(
-    ['projects/ejs/**/*.ejs','!' + 'projects/ejs/**/_*.ejs']
-  )
-  .pipe(ejs())
-  .pipe(gulp.dest('projects/public'))
-});
-
-//styledocco
-gulp.task('styledocco',function(){
-  gulp.src('styleguide/',function(){
-    return sass('styleguide/scss/',{
-      style: 'expanded',
-      compass: true,
-      sourcemap: true
-    })
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('styleguide/css/'))
     .pipe(styledocco({
-      out: 'styleguide/',
-      include: ['bootstrap/css/bootstrap.css'],
+      out: './',
       name: 'GuideLine'
     }))
   });
